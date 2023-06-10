886f5ccf6ddce903f88e3e3603d371895ba9caf3
gofinge <gofinge.98@gmail.com>
2023-06-10 15:31:11 +0800

    fix bug

--- tools/test.py
+++ tools/test.py
@@ -6,7 +6,6 @@ Please cite our work if the code is helpful to you.
 """
 
 import os
-import argparse
 import collections
 
 import torch
@@ -14,40 +13,17 @@ import torch.nn.parallel
 import torch.optim
 import torch.utils.data
 
+import pointcept.utils.comm as comm
+from pointcept.engines.defaults import default_argument_parser, default_config_parser, default_setup, create_ddp_model
+from pointcept.engines.launch import launch
 from pointcept.models import build_model
 from pointcept.datasets import build_dataset
-from pointcept.datasets.utils import collate_fn
-from pointcept.utils.config import Config, DictAction
 from pointcept.utils.logger import get_root_logger
-from pointcept.utils.env import get_random_seed, set_seed
 from pointcept.engines.test import TEST
 
 
-def get_parser():
-    parser = argparse.ArgumentParser(description='Pointcept Test Process')
-    parser.add_argument('--config-file', default="", metavar="FILE", help="path to config file")
-    parser.add_argument('--options', nargs='+', action=DictAction, help='custom options')
-    args = parser.parse_args()
-    return args
-
-
-def main():
-    args = get_parser()
-
-    # config_parser
-    cfg = Config.fromfile(args.config_file)
-    if args.options is not None:
-        cfg.merge_from_dict(args.options)
-
-    if cfg.seed is None:
-        cfg.seed = get_random_seed()
-
-    os.makedirs(cfg.save_path, exist_ok=True)
-
-    # default_setup
-    set_seed(cfg.seed)
-    cfg.batch_size_val_per_gpu = cfg.batch_size_test  # TODO: add support to multi gpu test
-    cfg.num_worker_per_gpu = cfg.num_worker  # TODO: add support to multi gpu test
+def main_worker(cfg):
+    cfg = default_setup(cfg)
 
     # tester init
     weight_name = os.path.basename(cfg.weight).split(".")[0]
@@ -56,21 +32,13 @@ def main():
     logger.info(f"Save path: {cfg.save_path}")
     logger.info(f"Config:\n{cfg.pretty_text}")
 
+    tester = TEST.build(cfg.test)
     # build model
     logger.info("=> Building model ...")
     model = build_model(cfg.model).cuda()
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     logger.info(f"Num params: {n_parameters}")
-
-    # build dataset
-    logger.info("=> Building test dataset & dataloader ...")
-    test_dataset = build_dataset(cfg.data.test)
-    test_loader = torch.utils.data.DataLoader(test_dataset,
-                                              batch_size=cfg.batch_size_val_per_gpu,
-                                              shuffle=False,
-                                              num_workers=cfg.num_worker_per_gpu,
-                                              pin_memory=True,
-                                              collate_fn=collate_fn)
+    model = create_ddp_model(model.cuda(), broadcast_buffers=False, find_unused_parameters=False)
 
     # load checkpoint
     if os.path.isfile(cfg.weight):
@@ -79,15 +47,48 @@ def main():
         new_state_dict = collections.OrderedDict()
         for name, value in state_dict.items():
             if name.startswith("module."):
-                name = name[7:]  # module.xxx.xxx -> xxx.xxx
+                if comm.get_world_size() == 1:
+                    name = name[7:]  # module.xxx.xxx -> xxx.xxx
+            else:
+                if comm.get_world_size() > 1:
+                    name = "module." + name  # xxx.xxx -> module.xxx.xxx
             new_state_dict[name] = value
         model.load_state_dict(new_state_dict, strict=True)
         logger.info("=> Loaded weight '{}' (epoch {})".format(cfg.weight, checkpoint['epoch']))
         cfg.test_epoch = checkpoint['epoch']
     else:
         raise RuntimeError("=> No checkpoint found at '{}'".format(cfg.weight))
-    TEST.build(cfg.test)(cfg, test_loader, model)
+
+    # build dataset
+    logger.info("=> Building test dataset & dataloader ...")
+    test_dataset = build_dataset(cfg.data.test)
+    if comm.get_world_size() > 1:
+        test_sampler = torch.utils.data.distributed.DistributedSampler(test_dataset)
+    else:
+        test_sampler = None
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                              batch_size=cfg.batch_size_test_per_gpu,
+                                              shuffle=False,
+                                              num_workers=cfg.batch_size_test_per_gpu,
+                                              pin_memory=True,
+                                              sampler=test_sampler,
+                                              collate_fn=tester.collate_fn)
+    tester(cfg, test_loader, model)
 
 
-if __name__ == '__main__':
+def main():
+    args = default_argument_parser().parse_args()
+    cfg = default_config_parser(args.config_file, args.options)
+
+    launch(
+        main_worker,
+        num_gpus_per_machine=args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        cfg=(cfg,),
+    )
+
+
+if __name__ == "__main__":
     main()
