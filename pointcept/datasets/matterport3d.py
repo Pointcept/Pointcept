3e102908bb06700d0ef5e5f96d474d3e18b11183
Gofinge <gofinge.98@gmail.com>
2024-09-07 13:53:01 -0700

    black format & rebase

--- pointcept/datasets/matterport3d.py
+++ pointcept/datasets/matterport3d.py
@@ -18,58 +18,96 @@ from .builder import DATASETS
 from .transform import Compose, TRANSFORMS
 
 # 20 classes in ScanNet + Ceiling
-MATTERPORT_LABELS_21 = ('wall', 'floor', 'cabinet', 'bed', 'chair', 'sofa', 'table', 'door',
-                    'window', 'bookshelf', 'picture', 'counter', 'desk', 'curtain', 'refrigerator',
-                    'shower curtain', 'toilet', 'sink', 'bathtub', 'other', 'ceiling')
+MATTERPORT_LABELS_21 = (
+    "wall",
+    "floor",
+    "cabinet",
+    "bed",
+    "chair",
+    "sofa",
+    "table",
+    "door",
+    "window",
+    "bookshelf",
+    "picture",
+    "counter",
+    "desk",
+    "curtain",
+    "refrigerator",
+    "shower curtain",
+    "toilet",
+    "sink",
+    "bathtub",
+    "other",
+    "ceiling",
+)
 
 
 @DATASETS.register_module()
 class Matterport3DDataset(Dataset):
-    class2id = np.array((1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21))
+    class2id = np.array(
+        (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21)
+    )
 
-    def __init__(self,
-                 split='train',
-                 data_root='data/matterport3d',
-                 transform=None,
-                 lr_file=None,
-                 la_file=None,
-                 ignore_index=-1,
-                 test_mode=False,
-                 test_cfg=None,
-                 cache=False,
-                 loop=1):
+    def __init__(
+        self,
+        split="train",
+        data_root="data/matterport3d",
+        transform=None,
+        lr_file=None,
+        la_file=None,
+        ignore_index=-1,
+        test_mode=False,
+        test_cfg=None,
+        cache=False,
+        loop=1,
+    ):
         super(Matterport3DDataset, self).__init__()
         self.data_root = data_root
         self.split = split
         self.transform = Compose(transform)
         self.cache = cache
-        self.loop = loop if not test_mode else 1  # force make loop = 1 while in test mode
+        self.loop = (
+            loop if not test_mode else 1
+        )  # force make loop = 1 while in test mode
         self.test_mode = test_mode
         self.test_cfg = test_cfg if test_mode else None
 
         if test_mode:
             self.test_voxelize = TRANSFORMS.build(self.test_cfg.voxelize)
-            self.test_crop = TRANSFORMS.build(self.test_cfg.crop) if self.test_cfg.crop else None
+            self.test_crop = (
+                TRANSFORMS.build(self.test_cfg.crop) if self.test_cfg.crop else None
+            )
             self.post_transform = Compose(self.test_cfg.post_transform)
             self.aug_transform = [Compose(aug) for aug in self.test_cfg.aug_transform]
 
         if lr_file:
-            self.data_list = [os.path.join(data_root, "train", name + ".pth") for name in
-                              np.loadtxt(lr_file, dtype=str)]
+            self.data_list = [
+                os.path.join(data_root, "train", name + ".pth")
+                for name in np.loadtxt(lr_file, dtype=str)
+            ]
         else:
             self.data_list = self.get_data_list()
         self.la = torch.load(la_file) if la_file else None
         self.ignore_index = ignore_index
         logger = get_root_logger()
-        logger.info("Totally {} x {} samples in {} set.".format(len(self.data_list), self.loop, split))
+        logger.info(
+            "Totally {} x {} samples in {} set.".format(
+                len(self.data_list), self.loop, split
+            )
+        )
 
     def get_data_list(self):
         if isinstance(self.split, str):
-            data_list = glob.glob(os.path.join(self.data_root, self.split, "*", "*.pth"))
+            data_list = glob.glob(
+                os.path.join(self.data_root, self.split, "*", "*.pth")
+            )
         elif isinstance(self.split, Sequence):
             data_list = []
             for split in self.split:
-                data_list += glob.glob(os.path.join(self.data_root, split, "*",  "*.pth"))
+                data_list += glob.glob(
+                    os.path.join(self.data_root, split, "*", "*.pth")
+                )
         else:
             raise NotImplementedError
         return data_list
@@ -79,7 +117,9 @@ class Matterport3DDataset(Dataset):
         if not self.cache:
             data = torch.load(data_path)
         else:
-            data_name = data_path.replace(os.path.dirname(self.data_root), '').split(".")[0]
+            data_name = data_path.replace(os.path.dirname(self.data_root), "").split(
+                "."
+            )[0]
             cache_name = "pointcept" + data_name.replace(os.path.sep, "-")
             data = shared_dict(cache_name)
         coord = data["coord"]
@@ -89,7 +129,14 @@ class Matterport3DDataset(Dataset):
         segment = data["semantic_gt21"].reshape([-1])
 
         instance = np.ones(coord.shape[0]) * -1
-        data_dict = dict(coord=coord, normal=normal, color=color, segment=segment, instance=instance, scene_id=scene_id)
+        data_dict = dict(
+            coord=coord,
+            normal=normal,
+            color=color,
+            segment=segment,
+            instance=instance,
+            scene_id=scene_id,
+        )
         if self.la:
             sampled_index = self.la[self.get_data_name(idx)]
             mask = np.ones_like(segment).astype(np.bool)
@@ -115,9 +162,7 @@ class Matterport3DDataset(Dataset):
         data_dict = self.transform(data_dict)
         data_dict_list = []
         for aug in self.aug_transform:
-            data_dict_list.append(
-                aug(deepcopy(data_dict))
-            )
+            data_dict_list.append(aug(deepcopy(data_dict)))
 
         input_dict_list = []
         for data in data_dict_list:
@@ -131,7 +176,9 @@ class Matterport3DDataset(Dataset):
 
         for i in range(len(input_dict_list)):
             input_dict_list[i] = self.post_transform(input_dict_list[i])
-        data_dict = dict(fragment_list=input_dict_list, segment=segment, name=self.get_data_name(idx))
+        data_dict = dict(
+            fragment_list=input_dict_list, segment=segment, name=self.get_data_name(idx)
+        )
         return data_dict
 
     def __getitem__(self, idx):
--- pointcept/datasets/preprocessing/matterport3d/preprocess_matterport3d_mesh.py
+++ pointcept/datasets/preprocessing/matterport3d/preprocess_matterport3d_mesh.py
@@ -2,6 +2,7 @@
 Preprocessing Matterport3D
 adatpted from https://github.com/pengsongyou/openscene/blob/main/scripts/preprocess/preprocess_3d_matterport.py
 """
+
 import os
 import argparse
 import glob
@@ -37,120 +38,195 @@ MATTERPORT_CLASS_REMAP[34] = 18
 MATTERPORT_CLASS_REMAP[36] = 19
 MATTERPORT_CLASS_REMAP[39] = 20
 
-MATTERPORT_LABELS_21 = ('wall', 'floor', 'cabinet', 'bed', 'chair', 'sofa', 'table', 'door',
-                    'window', 'bookshelf', 'picture', 'counter', 'desk', 'curtain', 'refrigerator',
-                    'shower curtain', 'toilet', 'sink', 'bathtub', 'other', 'ceiling')
+MATTERPORT_LABELS_21 = (
+    "wall",
+    "floor",
+    "cabinet",
+    "bed",
+    "chair",
+    "sofa",
+    "table",
+    "door",
+    "window",
+    "bookshelf",
+    "picture",
+    "counter",
+    "desk",
+    "curtain",
+    "refrigerator",
+    "shower curtain",
+    "toilet",
+    "sink",
+    "bathtub",
+    "other",
+    "ceiling",
+)
 MATTERPORT_ALLOWED_NYU_CLASSES = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 22, 24, 28, 33, 34, 36, 39]
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    14,
+    16,
+    22,
+    24,
+    28,
+    33,
+    34,
+    36,
+    39,
+]
+
 
 def handle_process(mesh_path, output_path, mapping, train_scenes, val_scenes):
     # Get the scene id and region name from the mesh path
     scene_id = Path(mesh_path).parent.parent.name
     region_name = Path(mesh_path).stem
-    
+
     # Check which split the scene belongs to (train, val, or test)
     if scene_id in train_scenes:
-        output_folder = os.path.join(output_path, 'train', scene_id)
-        output_file = os.path.join(output_path, 'train', scene_id, f'{region_name}.pth')
-        split_name = 'train'
+        output_folder = os.path.join(output_path, "train", scene_id)
+        output_file = os.path.join(output_path, "train", scene_id, f"{region_name}.pth")
+        split_name = "train"
     elif scene_id in val_scenes:
-        output_folder = os.path.join(output_path, 'val', scene_id)
-        output_file = os.path.join(output_path, 'val', scene_id, f'{region_name}.pth')
-        split_name = 'val'
+        output_folder = os.path.join(output_path, "val", scene_id)
+        output_file = os.path.join(output_path, "val", scene_id, f"{region_name}.pth")
+        split_name = "val"
     else:
-        output_folder = os.path.join(output_path, 'test', scene_id)
-        output_file = os.path.join(output_path, 'test', scene_id, f'{region_name}.pth')
-        split_name = 'test'
-        
+        output_folder = os.path.join(output_path, "test", scene_id)
+        output_file = os.path.join(output_path, "test", scene_id, f"{region_name}.pth")
+        split_name = "test"
+
     # Create the output directory if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
-    print(f'Processing: {scene_id} in {split_name}')
-    
+    print(f"Processing: {scene_id} in {split_name}")
+
     # Load the vertex data
-    with open(mesh_path, 'rb') as f:
+    with open(mesh_path, "rb") as f:
         plydata = plyfile.PlyData.read(f)
-    vertex_data = plydata['vertex'].data
+    vertex_data = plydata["vertex"].data
 
     # Get the coordinates, colors, and normals from the vertex data
-    coords = np.vstack([vertex_data['x'], vertex_data['y'], vertex_data['z']]).T
-    colors = np.vstack([vertex_data['red'], vertex_data['green'], vertex_data['blue']]).T
-    normals = np.vstack([vertex_data['nx'], vertex_data['ny'], vertex_data['nz']]).T
-    
+    coords = np.vstack([vertex_data["x"], vertex_data["y"], vertex_data["z"]]).T
+    colors = np.vstack(
+        [vertex_data["red"], vertex_data["green"], vertex_data["blue"]]
+    ).T
+    normals = np.vstack([vertex_data["nx"], vertex_data["ny"], vertex_data["nz"]]).T
+
     # Prepare the data to be saved
-    save_dict = dict(coord=coords.astype("float32"), color=colors.astype("uint8"), normal=normals.astype("float32"), scene_id=scene_id+"_"+region_name)
+    save_dict = dict(
+        coord=coords.astype("float32"),
+        color=colors.astype("uint8"),
+        normal=normals.astype("float32"),
+        scene_id=scene_id + "_" + region_name,
+    )
 
     # Load the face data
-    face_data = plydata['face'].data
-    category_id = face_data['category_id']
-    
+    face_data = plydata["face"].data
+    category_id = face_data["category_id"]
+
     # Replace -1 with 0 in category_id
-    category_id[category_id==-1] = 0
-    
+    category_id[category_id == -1] = 0
+
     # Map the labels according to NYU40ID
     mapped_labels = mapping[category_id]
-    
+
     # Replace labels not in MATTERPORT_ALLOWED_NYU_CLASSES with 0
-    mapped_labels[np.logical_not(
-            np.isin(mapped_labels, MATTERPORT_ALLOWED_NYU_CLASSES))] = 0
-    
+    mapped_labels[
+        np.logical_not(np.isin(mapped_labels, MATTERPORT_ALLOWED_NYU_CLASSES))
+    ] = 0
+
     # Remap the labels to ScanNet 20 categories + ceiling
     remapped_labels = MATTERPORT_CLASS_REMAP[mapped_labels].astype(int)
-    
+
     # Calculate per-vertex labels
-    triangles = face_data['vertex_indices']
+    triangles = face_data["vertex_indices"]
     vertex_labels = np.zeros((coords.shape[0], 22), dtype=np.int32)
     # calculate per-vertex labels
     for row_id in range(triangles.shape[0]):
         for i in range(3):
-            vertex_labels[triangles[row_id][i],
-                            remapped_labels[row_id]] += 1
+            vertex_labels[triangles[row_id][i], remapped_labels[row_id]] += 1
 
     # Get the most frequent label for each vertex
     vertex_labels = np.argmax(vertex_labels, axis=1)
     vertex_labels -= 1
-    
+
     # Add the vertex labels to the data to be saved
     save_dict["semantic_gt21"] = vertex_labels.astype("int16")
-    
 
     # Save processed data
     torch.save(save_dict, output_file)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_root', required=True, help='Path to the Matterport3D dataset containing scene folders')
-    parser.add_argument('--output_root', required=True, help='Output path where train/val folders will be located')
+    parser.add_argument(
+        "--dataset_root",
+        required=True,
+        help="Path to the Matterport3D dataset containing scene folders",
+    )
+    parser.add_argument(
+        "--output_root",
+        required=True,
+        help="Output path where train/val folders will be located",
+    )
     opt = parser.parse_args()
-    
+
     # Load label map
-    category_mapping = pd.read_csv('pointcept/datasets/preprocessing/matterport3d/meta_data/category_mapping.tsv',\
-                        sep='\t', header=0)
-    mapping = np.insert(category_mapping[['nyu40id']].to_numpy()
-                        .astype(int).flatten(), 0, 0, axis=0)
+    category_mapping = pd.read_csv(
+        "pointcept/datasets/preprocessing/matterport3d/meta_data/category_mapping.tsv",
+        sep="\t",
+        header=0,
+    )
+    mapping = np.insert(
+        category_mapping[["nyu40id"]].to_numpy().astype(int).flatten(), 0, 0, axis=0
+    )
 
     # Load train/val splits
-    with open('pointcept/datasets/preprocessing/matterport3d/meta_data/train.txt') as train_file:
+    with open(
+        "pointcept/datasets/preprocessing/matterport3d/meta_data/train.txt"
+    ) as train_file:
         train_scenes = train_file.read().splitlines()
-    with open('pointcept/datasets/preprocessing/matterport3d/meta_data/val.txt') as val_file:
+    with open(
+        "pointcept/datasets/preprocessing/matterport3d/meta_data/val.txt"
+    ) as val_file:
         val_scenes = val_file.read().splitlines()
-    with open('pointcept/datasets/preprocessing/matterport3d/meta_data/test.txt') as test_file:
+    with open(
+        "pointcept/datasets/preprocessing/matterport3d/meta_data/test.txt"
+    ) as test_file:
         test_scenes = test_file.read().splitlines()
 
     # Create output directories
     os.makedirs(opt.output_root, exist_ok=True)
-    train_output_dir = os.path.join(opt.output_root, 'train')
+    train_output_dir = os.path.join(opt.output_root, "train")
     os.makedirs(train_output_dir, exist_ok=True)
-    val_output_dir = os.path.join(opt.output_root, 'val')
+    val_output_dir = os.path.join(opt.output_root, "val")
     os.makedirs(val_output_dir, exist_ok=True)
-    test_output_dir = os.path.join(opt.output_root, 'test')
+    test_output_dir = os.path.join(opt.output_root, "test")
     os.makedirs(test_output_dir, exist_ok=True)
-    
+
     # Load scene paths
-    scene_paths = sorted(glob.glob(opt.dataset_root + '/*/region_segmentations/*.ply'))
+    scene_paths = sorted(glob.glob(opt.dataset_root + "/*/region_segmentations/*.ply"))
 
     # Preprocess data.
     pool = ProcessPoolExecutor(max_workers=mp.cpu_count())
     # pool = ProcessPoolExecutor(max_workers=1)
-    print('Processing scenes...')
-    _ = list(pool.map(handle_process, scene_paths, repeat(opt.output_root), repeat(mapping), repeat(train_scenes),
-                      repeat(val_scenes)))
+    print("Processing scenes...")
+    _ = list(
+        pool.map(
+            handle_process,
+            scene_paths,
+            repeat(opt.output_root),
+            repeat(mapping),
+            repeat(train_scenes),
+            repeat(val_scenes),
+        )
+    )
